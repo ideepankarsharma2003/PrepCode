aa10eebb2568d32ebb931a92196861ebac524576
Lucky <deepankarsharma2003@gmail.com>
2022-02-02 21:02:29 +0530

    unique email

--- LeetCode/Google/01-Interview-Process/uniqueEmails.py
+++ LeetCode/Google/01-Interview-Process/uniqueEmails.py
@@ -1,11 +1,31 @@
-from signal import SIG_DFL
-
-
-def numUniqueEmails(self, emails):
+class Solution(object):
+    def numUniqueEmails(self, emails):
         """
         :type emails: List[str]
         :rtype: int
         """
-        count= 0
+
+        s = set()
+        count=0
         for email in emails:
-            
+            ind = email.index('@')
+            name = email[:ind]
+            domain = email[ind:]
+            name = name.replace('.', '')
+            if '+' in name:
+                pInd = name.index('+')
+                name = name[:pInd]
+            email = name+domain
+            if email not in s:
+                s.add(email)
+                # print(email)
+                count+=1
+        # print(count)
+        return count
+
+
+
+emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]
+obj = Solution()
+obj.numUniqueEmails(emails)
+

3895f8b6911179a8d1204a3865c901946d55c930
Lucky <deepankarsharma2003@gmail.com>
2022-05-11 08:21:42 +0530

    week 02 started

---
+++ Practice/Nancy/vsGurpreet/Hackerrank/3-month-prep/week 02/LonelyInteger.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+
+def lonelyinteger(a):
+    # Write your code here
+    dict = {}
+    for i in (a):
+        if i not in dict.keys():
+            dict[i] = 1
+        else:
+            dict[i] += 1
+    for i in dict.keys():
+        if dict[i] == 1:
+            return i
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

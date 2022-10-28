52a8c072a3cc234c252bebffb3760f12915312b4
Deepankar Sharma <deepankarsharma2003@gmail.com>
2022-10-28 19:21:51 +0530

    added notes

---
+++ Concepts-Clarity/Cpp/GFG/module 01/03_InputOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief Input Output
+ *
+ * iostream --->   {I/O streams}        --->    cin, cout, cerr
+ * iomanip  --->   {I/O manipulators}   --->    setw, setprecision
+ * fstream  --->   {File streams}       --->    
+ */
+
+int main()
+{
+    cerr<< "An Error occured"<<endl;
+
+    return 0;
+}

5c26da84f36620240b64f18969271306e34baa5c
Lucky <deepankarsharma2003@gmail.com>
2023-03-12 20:53:29 +0530

    added w2 assignment

--- Concepts-Clarity/Data Science/2023/Data Analysis/R-Programming/Temp.r
+++ Concepts-Clarity/Data Science/2023/Data Analysis/R-Programming/Temp.r
@@ -1,8 +1,24 @@
-library(palmerpenguins)
-summary(penguins)
-View(penguins)
+# library(palmerpenguins)
+# summary(penguins)
+# View(penguins)
 
 
 
 
-# library(ggplot2)
+# # library(ggplot2)
+
+
+library(tidyverse)
+library(Tmisc)
+data(quartet)
+# Anscombe's quartet ---> Four datasets that have nearly identical summary statistics
+
+quartet %>%
+    group_by(set) %>%
+    summarise(mean = mean(x), sd(x), mean(y), sd(y), cor(x, y))
+
+
+ggplot(quartet, aes(x, y)) +
+    geom_point() +
+    geom_smooth(method = lm, se = FALSE) +
+    facet_wrap(~set)
